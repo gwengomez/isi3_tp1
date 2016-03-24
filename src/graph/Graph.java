d82aa562e4ee4e05e132c969874c19b5f50b183d
Epulapp <Epulapp@EPULAPP54.univ-lyon1.fr>
2016-03-24 14:49:55 +0100

    Question 1 : fait

---
+++ src/graph/Graph.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package graph;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ *
+ * @author Epulapp
+ */
+public class Graph implements IDirectedGraph{
+    
+    private List<Node> nodes;
+    private Map<Node, List<Arc>> arcs;
+
+    public Graph() {
+        this.nodes = new ArrayList<>();
+        this.arcs = new HashMap<>();
+    }   
+
+    @Override
+    public boolean hasArc(Node _n1, Node _n2) {
+        List<Arc> outgoing = arcs.get(_n1);
+        if(outgoing == null) {
+            return false;
+        }
+        for(Arc c : outgoing) {
+            if(c.getDestination() == _n2) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public void addArc(Arc _edge) {
+        arcs.get(_edge.getSource()).add(_edge);
+    }
+
+    @Override
+    public List<Arc> getArc(Node _n) {
+        return arcs.get(_n);
+    }
+
+    @Override
+    public void addNode(Node _node) {
+        nodes.add(_node);
+        arcs.put(_node, new ArrayList<Arc>());
+    }
+
+    @Override
+    public List<Node> getAllNodes() {
+        return nodes;
+    }
+
+    @Override
+    public int getNbNodes() {
+        return nodes.size();
+    }
+
+    @Override
+    public List<Node> getAdjNodes(Node _n) {
+        List<Node> adjacents = new ArrayList<Node>();
+        for(List<Arc> lsArcs : arcs.values()) {
+            for(Arc c : lsArcs) {
+                if(c.getSource() == _n && !adjacents.contains(c.getDestination())) {
+                    adjacents.add(c.getDestination());
+                } else if(c.getDestination() == _n && !adjacents.contains(c.getSource())) {
+                    adjacents.add(c.getSource());
+                }
+            }
+        }
+        return adjacents;
+    }
+    
+}
