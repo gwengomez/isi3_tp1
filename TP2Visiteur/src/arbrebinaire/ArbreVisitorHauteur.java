d7a63a6f9fbb11085d3b49181df702224e4ebb97
Epulapp <Epulapp@EPULAPP54.univ-lyon1.fr>
2016-03-25 10:54:31 +0100

    TP2 finit

--- TP2Visiteur/src/arbrebinaire/ArbreVisitorHauteur.java
+++ TP2Visiteur/src/arbrebinaire/ArbreVisitorHauteur.java
@@ -11,43 +11,46 @@ package arbrebinaire;
  */
 public class ArbreVisitorHauteur implements ArbreVisitor{
     
-    private int hauteurCourante = 0;
-    private int hauteurMax = 0;
+    private int hauteur;
 
     @Override
     public void visitNegation(Negation n) {
-        this.hauteurCourante +=1;
+        this.hauteur += 1;
         n.getOpG().accept(this);
-        this.hauteurCourante -= 1;
+        
     }
 
     @Override
     public void visitAddition(Addition n) {
-        parcoursHauteur(n);
+        parcoursHauteurBinaire(n);
     }
 
     @Override
     public void visitMultiplication(Multiplication n) {
-        parcoursHauteur(n);
+        parcoursHauteurBinaire(n);
     }
 
     @Override
     public void visitConstante(Constante n) {
-        this.hauteurCourante += 1;
-        if(this.hauteurCourante > this.hauteurMax) {
-            this.hauteurMax = this.hauteurCourante;
-        }
-        this.hauteurCourante -= 1;
-    }
-    
-    private void parcoursHauteur(OperateurBinaire n) {
-        this.hauteurCourante += 1;
-        n.getOpG().accept(this);
-        n.getOpD().accept(this);
-        this.hauteurCourante -= 1;
+        this.hauteur = 1;
     }
 
     public int getHauteur() {
-        return hauteurMax;
-    }    
+        return hauteur;
+    }
+    
+    private void parcoursHauteurBinaire(OperateurBinaire n) {
+        int _hauteur = this.hauteur;
+        n.getOpG().accept(this);
+        int hauteurG = this.hauteur;
+        this.hauteur = _hauteur;
+        n.getOpD().accept(this);
+        int hauteurD = this.hauteur;
+        
+        if(hauteurG > hauteurD) {
+            this.hauteur = hauteurG + 1;
+        } else {
+            this.hauteur = hauteurD + 1;
+        }
+    }
 }
